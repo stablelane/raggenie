1800c47d56b5c45323b5bef83fba143d67355ed0
stablelane <aaanshad09@gmail.com>
2025-07-02 19:12:12 +0530

    Update docling version to 2.39.0 and clear default backend URL in configuration

--- ui/src/config/const.js
+++ ui/src/config/const.js
@@ -1,4 +1,4 @@
-const defaultBackednURL = "http://0.0.0.0:8001"
+const defaultBackednURL = ""
 export const  API_URL = (import.meta.env.VITE_BACKEND_URL ?? defaultBackednURL) + "/api/v1"
 export const  BACKEND_SERVER_URL = import.meta.env.VITE_BACKEND_URL ?? defaultBackednURL
 export const  SLACK_URL = "https://theailounge.slack.com"
